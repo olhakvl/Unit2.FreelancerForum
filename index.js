0b5899516e6446e6605df95788fff036484b0d10
olhakvl <olhakoval21@gmail.com>
2023-11-27 15:11:50 -0600

    added functions to render the initial table and calculate average

--- index.js
+++ index.js
@@ -2,87 +2,64 @@
 // Here, we define variables for the data that our program needs to remember.
 // We call this data "state" because it represents the state of our program.
 // freelancers array
-const freelancers = [
+const initialFreelancersTable = [
     { name: "Dr. Slice", price: 25, occupation: "gardener" },
     { name: "Dr. Pressure", price: 51, occupation: "programmer" },
     { name: "Prof. Possibility", price: 43, occupation: "teacher" },
-    { name: "Prof. Prism", price: 81, occupation: "teacher" },
-    // { name: "Dr. Impulse", price: 43, occupation: "teacher" },
-    // { name: "Prof. Spark", price: 76, occupation: "programmer" },
-    // { name: "Dr. Wire", price: 47, occupation: "teacher" },
-    // { name: "Prof. Goose", price: 72, occupation: "driver" },
   ];
 
+  const randomData = [
+    { name: "Prof. Prism", price: 81, occupation: "teacher" },
+    { name: "Dr. Impulse", price: 43, occupation: "teacher" },
+    { name: "Prof. Spark", price: 76, occupation: "programmer" },
+    { name: "Dr. Wire", price: 47, occupation: "teacher" },
+    { name: "Prof. Goose", price: 72, occupation: "driver" },
+    { name: "Amy", price: 55, occupation: "baker" },
+    { name: "Anna", price: 62, occupation: "accountant" }
+  ]
 
+  // function to render the initial freelancers table
+  function renderTable(initialTable) {
+    // save html table element to a variable
+    let htmlTable = document.querySelector("#freelancerTable");
 
-
-
-
-
-
-
-
-
-  /* State */
-// Here, we define variables for the data that our program needs to remember.
-// We call this data "state" because it represents the state of our program.
-
-
-// `setInterval` will call `addShape` every 1000 milliseconds (1 second)
-// and return an interval ID that we can use to stop the interval later.
-// Calling `clearInterval(addShapeIntervalId)` will stop the interval.
-const addShapeIntervalId = setInterval(addShape, 1000);
-
-render(); // We call this function once to render the initial state
-
-/**
- * Update the DOM to reflect the current state.
- * The term "render" is often used to describe this process.
- */
-function render() {
-  // Render the squares
-  const squares = document.querySelector("#squares");
-  const squareElements = shapes.map((shape) => {
-    const element = document.createElement("li");
-    element.classList.add(shape.color, shape.size);
-    return element;
-  });
-  squares.replaceChildren(...squareElements);
-
-  // TODO: Render the circles
-  // const circles = document.querySelector("#circle");
-  // circles.replaceChildren();
-  // shapes.forEach((shape) => {
-  //   const circle = document.createElement("li");
-  //   circle.classList.add("circle", shape.color, shape.size);
-  //   circles.append(circle);
-  // });
-
-  const circles = document.querySelector("#circle");
-  const circleElements = shapes.map((shape) => {
-    const element = document.createElement("li");
-    element.classList.add("circle", shape.color, shape.size);
-    return element;
-  });
-  circles.replaceChildren(...circleElements);
-}
-
-/**
- * Add a random shape to the `shapes` array
- */
-function addShape() {
-  const color = colors[Math.floor(Math.random() * colors.length)];
-  console.log(color);
-
-  // TODO: Randomize the size of the shape
-  const size = sizes[Math.floor(Math.random() * sizes.length)];
-  console.log(size);
-  shapes.push({ color, size });
-
-  render();
-
-  // TODO: Stop adding shapes if we've reached the maximum number of shapes
-  if (shapes.length >= maxShapes) {
-    clearInterval(addShapeIntervalId);
+    // loop throgh the initialFreelancersTable, create a new row and add corresponding data from the initialFreelancersTable into the cells
+    initialTable.forEach(currentValue => {
+      let newRow = htmlTable.insertRow();
+      let nameCell = newRow.insertCell(0);
+      let occupationCell = newRow.insertCell(1);
+      let priceCell = newRow.insertCell(2);
+      nameCell.textContent = currentValue.name;
+      occupationCell.textContent = currentValue.occupation;
+      priceCell.textContent = `$${currentValue.price}`;
+    });
   }
+
+  renderTable(initialFreelancersTable);
+
+// function to calculate average price
+function averagePrice(tableData){
+  // get sum of all prices
+  let totalPrice = tableData.reduce((total, currentRow) => total += currentRow.price, 0
+  );
+
+  // get average of prices
+  let averagePrice = totalPrice / tableData.length;
+  return Math.floor(averagePrice);
 }
+
+// set the average price in the html
+let htmlPriceAverage = document.querySelector("#averagePrice");
+let initialAveragePrice = averagePrice(initialFreelancersTable);
+htmlPriceAverage.append(initialAveragePrice);
+
+// function to generate a new random freelancer
+function randomFreelancerData(){
+
+}
+
+      // create a new row
+    // let newRow = document.createElement("tr");
+    // // name cell
+    // let nameCell = document.createElement("td"); // create name cell
+    // nameCell.innerHTML = initialTable[i].name;
